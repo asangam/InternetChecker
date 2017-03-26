8eba9c91de2979dc5d6c23d62635b021c19aee05
¨asangam¨ <asangam@outlook.com>
2017-03-26 11:18:15 +0545

    Checks Internet Connection
    A simole app written in C# based on ping which checks if you have an
    active internet connection or not .

---
+++ InternetChecker/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace InternetChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblWorking = new System.Windows.Forms.Label();
+            this.lblNotWorking = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(41, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Check Network";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblWorking
+            // 
+            this.lblWorking.AutoSize = true;
+            this.lblWorking.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWorking.ForeColor = System.Drawing.Color.Red;
+            this.lblWorking.Location = new System.Drawing.Point(87, 145);
+            this.lblWorking.Name = "lblWorking";
+            this.lblWorking.Size = new System.Drawing.Size(87, 24);
+            this.lblWorking.TabIndex = 1;
+            this.lblWorking.Text = "Working";
+            this.lblWorking.Visible = false;
+            // 
+            // lblNotWorking
+            // 
+            this.lblNotWorking.AutoSize = true;
+            this.lblNotWorking.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNotWorking.ForeColor = System.Drawing.Color.Brown;
+            this.lblNotWorking.Location = new System.Drawing.Point(71, 185);
+            this.lblNotWorking.Name = "lblNotWorking";
+            this.lblNotWorking.Size = new System.Drawing.Size(125, 24);
+            this.lblNotWorking.TabIndex = 2;
+            this.lblNotWorking.Text = "Not Working";
+            this.lblNotWorking.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblNotWorking);
+            this.Controls.Add(this.lblWorking);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Internet Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblWorking;
+        private System.Windows.Forms.Label lblNotWorking;
+    }
+}
+
---
+++ InternetChecker/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InternetChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ping p = new Ping();
+            PingReply result = p.Send("8.8.8.8");
+            if(result.Status.ToString()=="Success")
+            {
+                //MessageBox.Show("Status :  " + result.Status + " \n Time : " + result.RoundtripTime.ToString() + " \n Address : " + result.Address);
+                //MessageBox.Show("Internet is working");
+                lblWorking.Visible = true;
+                lblNotWorking.Visible = false;
+
+            }
+            else
+            {
+               // MessageBox.Show("Internet is not working");
+                lblNotWorking.Visible = true;
+                lblWorking.Visible = false;
+            }
+
+        }
+    }
+}
